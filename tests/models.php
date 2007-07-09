8f1ed9b71363a08b52c2f6a626ed959347d56972
gavin <gavin@c7e42c7a-2b33-0410-b683-f3051610de63>
2007-07-09 08:42:46 +0000

    work on unit testing
    git-svn-id: file:///home/gavin/dev/.repos/pedantic/lib/trunk@46 c7e42c7a-2b33-0410-b683-f3051610de63

---
+++ tests/models.php
@@ -0,0 +1,81 @@
+<?
+class customer extends AR
+{
+    function connect_to_db()
+    {
+        $dsn = array(
+            'phptype' => 'mysql',
+            'username' => 'root',
+            'password' => '',
+            'hostspec' => 'localhost',
+            'database' => 'ARTest'
+        );
+        $this->db =& MDB2::Connect($dsn);
+    }
+}
+
+#a fake mock thingy
+class App
+{
+    function error_check($result, $die_on_error = true)
+    {
+        if (PEAR::isError($result) || MDB2::isError($result)) {
+            if ($die_on_error)
+            {
+                die('<pre>'.$result->getMessage().' - '.$result->getUserinfo()).'</pre>';
+            }
+            else
+            {
+                return $result->code;
+            }
+        }
+    }
+    
+    static $schema_definition = array(
+    'customer' => Array(
+	'id' => Array(
+		'type' => 'int',
+		'mdb2type' => 'integer',
+		'length' => '4',
+		'default' => ''
+	),
+	'created_on' => Array(
+		'type' => 'datetime',
+		'mdb2type' => 'timestamp',
+		'length' => '',
+		'default' => ''
+	),
+	'updated_on' => Array(
+		'type' => 'datetime',
+		'mdb2type' => 'timestamp',
+		'length' => '',
+		'default' => ''
+	),
+	'name' => Array(
+		'type' => 'varchar',
+		'mdb2type' => 'text',
+		'length' => '255',
+		'default' => ''
+	),
+	'company_name' => Array(
+		'type' => 'varchar',
+		'mdb2type' => 'text',
+		'length' => '255',
+		'default' => ''
+	),
+	'address' => Array(
+		'type' => 'text',
+		'mdb2type' => 'text',
+		'length' => '',
+		'default' => ''
+	),
+	'active' => Array(
+		'type' => 'char',
+		'mdb2type' => 'text',
+		'length' => '1',
+		'default' => 'Y'
+	)
+        ));
+
+}
+?>
