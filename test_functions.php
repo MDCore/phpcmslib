d7d6221b75ecbfa4e4939a01b99efce8301da2ac
gavin <gavin@c7e42c7a-2b33-0410-b683-f3051610de63>
2007-07-28 10:31:59 +0000

    added some test functions into lib, for use in the actual application. messy at the moment, but I'll clean it up
    git-svn-id: file:///home/gavin/dev/.repos/pedantic/lib/trunk@94 c7e42c7a-2b33-0410-b683-f3051610de63

--- test_functions.php
+++ test_functions.php
@@ -2,6 +2,63 @@
 /*
  * todo this code is copied verbatim from the migration code in system admin! Clean that up
  */
+
+class test
+{
+    public function default_setup()
+    {
+    $_POST = null; $_GET = null;
+    #remigrate me!!
+    #parse the migrations folder
+        $path = App::$env->root.'/db/migrations';
+
+        if ($handle = opendir(App::$env->root.'/db/migrations'))
+        {
+            $migrations = Array();
+            while (false != ($file = readdir($handle)))
+            {
+                $file = $path."/$file";
+                if (is_file($file))
+                {
+                    $migrations[] = $file;
+                }
+            }
+            closedir($handle);
+        }
+
+        #sort the array
+            sort($migrations);
+        
+        #build the meta-data
+        for ( $i=0; $i < sizeof($migrations); $i++ )
+        { 
+            $file_name = file_name($migrations[$i]);
+            $version = explode('_', $file_name); $version = $version[0]; 
+            $description = str_replace('_', ' ', str_replace(file_extension($file_name), '', substr($file_name, strlen($version)+1)));
+            $migrations[$i] = array(
+                'version' => (int)$version,
+                'filename' => $migrations[$i],
+                'description' => $description,
+                'extension' => file_extension($migrations[$i]),
+            );
+        }
+        foreach ($migrations as $migration_file)
+        {
+            switch ($migration_file['extension'])
+            {
+            case 'sql':
+                $sql_migration = file_get_contents($migration_file['filename']);
+                execute_many_sql_statements($sql_migration, false);
+                break;
+            case 'php';
+                    require($migration_file['filename']);
+                break;
+            }
+            update_schema_version($migration_file['version']);
+        }
+    }
+}
+
 function update_schema_version($version)
 {
     $sql = "UPDATE schema_info set version='$version'";
