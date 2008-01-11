b9335cfce8033ecfbfdb3d2fc4775f7f05a141a1
gavin <gavin@c7e42c7a-2b33-0410-b683-f3051610de63>
2008-01-11 13:50:52 +0000

    added option
    git-svn-id: file:///home/gavin/dev/.repos/pedantic/lib/trunk@197 c7e42c7a-2b33-0410-b683-f3051610de63

--- init.php
+++ init.php
@@ -21,7 +21,9 @@ if (isset($use_PEAR_in_vendor) && $use_PEAR_in_vendor) {
 
 /* PEAR Libraries */
 require_once('MDB2.php');
-require_once('DB/NestedSet.php');
+if (!isset($uses_nested_set) | (isset($uses_nested_set) && $uses_nested_set == true)) {
+    require_once('DB/NestedSet.php');
+}
 
 /* core classes */
 require("AR.php");
