9e8c4d8c8cda7abe71fbfb1e7aa9096a9fbfff69
gavin <gavin@c7e42c7a-2b33-0410-b683-f3051610de63>
2008-02-29 10:48:21 +0000

    removed bad date_add function
    -
    
    git-svn-id: file:///home/gavin/dev/.repos/pedantic/lib/trunk@213 c7e42c7a-2b33-0410-b683-f3051610de63

--- functions.php
+++ functions.php
@@ -197,65 +197,6 @@ if (!function_exists('property_exists')) {
    return array_key_exists($property, get_class_vars($class));
   }
 }
-function date_add($interval,$number,$dateTime) {
-       
-    $dateTime = (strtotime($dateTime) != -1) ? strtotime($dateTime) : $dateTime;      
-    $dateTimeArr=getdate($dateTime);
-               
-    $yr=$dateTimeArr[year];
-    $mon=$dateTimeArr[mon];
-    $day=$dateTimeArr[mday];
-    $hr=$dateTimeArr[hours];
-    $min=$dateTimeArr[minutes];
-    $sec=$dateTimeArr[seconds];
-
-    switch($interval) {
-        case "s"://seconds
-            $sec += $number;
-            break;
-
-        case "n"://minutes
-            $min += $number;
-            break;
-
-        case "h"://hours
-            $hr += $number;
-            break;
-
-        case "d"://days
-            $day += $number;
-            break;
-
-        case "ww"://Week
-            $day += ($number * 7);
-            break;
-
-        case "m": //similar result "m" dateDiff Microsoft
-            $mon += $number;
-            break;
-
-        case "yyyy": //similar result "yyyy" dateDiff Microsoft
-            $yr += $number;
-            break;
-
-        default:
-            $day += $number;
-         }      
-               
-        $dateTime = mktime($hr,$min,$sec,$mon,$day,$yr);
-        $dateTimeArr=getdate($dateTime);
-       
-        $nosecmin = 0;
-        $min=$dateTimeArr[minutes];
-        $sec=$dateTimeArr[seconds];
-
-        if ($hr==0){$nosecmin += 1;}
-        if ($min==0){$nosecmin += 1;}
-        if ($sec==0){$nosecmin += 1;}
-       
-        #if ($nosecmin>2){     return(date("Y-m-d",$dateTime));} else { return(date("Y-m-d G:i:s",$dateTime));}
-        return $dateTime;
-}
 
 function url_to($path, $include_base = true, $explicit_path = false) {
     //$url = App::$env->url;
