6821d5e754e3af0bd7f3074ae53c1f804ccb7630
Gavin van Lelyveld <gavin@pedantic.co.za>
2008-03-11 17:18:23 +0200

    cleaned up the migration html
    as above

--- tasks/migrate/migrate_html_layout.php
+++ tasks/migrate/migrate_html_layout.php
@@ -5,65 +5,40 @@
 <title>pedantic system admin - migrate</title>
 <?=default_scripts();?>
 <style type="text/css">
-body
-{
+body {
     font-family: tahoma, verdana, arial;
 }
-.migration
-{
+.migration {
     margin-bottom: 10px;
 }
-.migration h4
-{
+.migration h4 {
     margin: 0; padding: 0;
     font-weight: normal;
 }
-.migration a
-{
+.migration a {
     color: #000;
     font-size: 0.8em; 
     text-decoration: none;
 }
-.detail
-{
-    font-family: "courier new", monospace;
-    display: none;
-    border: 1px solid #ccc;
-    font-size: 10pt;
-    white-space: pre;
-}
-.detail div
-{
-    margin-bottom: 5px;
-}
-.success
-{
+.success {
     display: block;
     margin: 0; padding: 0;
 }
 </style>
-<script type="text/javascript">
-function show_hide_detail(detail_id)
-{
-    $('#'+detail_id).slidetoggle('fast');
-}
-</script>
 </head>
 <body>
-    <h2><?=proper_nounize(APP_NAME);?> schema is currently at version <?=$schema_version;?></h2><?
+    <p><?=proper_nounize(APP_NAME);?> schema is currently at version <?=$schema_version;?></p><?
 
     foreach ($sys->migrations as $migration)
     {
         if ($migration['version'] > $schema_version)
         {
             ?><div class="migration"><span class="description">version <?=$migration['version']?> - <i><?=$migration['description'];?></i></span>
-            <a href="#" onclick="show_hide_detail('v_<?=$migration['version'];?>_detail'); return false;">(detail)</a>
-                <div class="detail" id="v_<?=$migration['version'];?>_detail"><?
-            ?></div>
             <span class="success">successfully migrated to version <?=$migration['version'];?></span>&nbsp;
             </div><?
         }
     }
+    <p><?=proper_nounize(APP_NAME);?> schema has been migrated to version <?=$migration['version'];?></p><?
 
     echo $schema_interrregator_results;
     #successfully completed
