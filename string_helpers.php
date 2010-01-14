6863cac8b3681dd56d23d83ccd450b362b254ee2
Gavin van Lelyveld <gavin@pedantic.co.za>
2010-01-14 11:24:31 +0200

    Fixed a bug in singularize
    -es was not singularizing correctly

--- string_helpers.php
+++ string_helpers.php
@@ -63,7 +63,8 @@ function singularize($str) {
     if (preg_match('/ies/', $str)) { return substr($str, 0, strlen($str)-3).'y'; }
     if (preg_match('/sses/', $str)) { return substr($str, 0, strlen($str)-2); }
     if (preg_match('/ches/', $str)) { return substr($str, 0, strlen($str)-2); }
-    if (preg_match('/es/', $str)) { return substr($str, 0, strlen($str)-2); }
+    if (preg_match('/shes/', $str)) { return substr($str, 0, strlen($str)-2); }
+    if (preg_match('/es/', $str)) { return substr($str, 0, strlen($str)-1); }
     if (preg_match('/s/', $str)) { return substr($str, 0, strlen($str)-1); }
 
     return $str;
